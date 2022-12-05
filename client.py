3b90c9d402b293f011f4f774fb58e9e4cfae9521
Mikhail Sokolov <sokolovma@sweep-net.ru>
2022-12-06 01:04:48 +0300

    First build. All messages go to the public chat

--- client.py
+++ client.py
@@ -1,10 +1,47 @@
-# import asyncio
-# import aiohttp
+import asyncio
+
+from aioconsole import ainput
+
+from constants import HOST, PORT, DISCONNECT
 
 
 class Client:
-    def __init__(self, server_host="127.0.0.1", server_port=8000):
-        pass
+    def __init__(self, server_host: str = HOST, server_port: int = PORT):
+        self.server_host = server_host
+        self.server_port = server_port
+        self.disconnect: bool = False
 
-    def send(self, message=""):
-        pass
+    async def start_client(self):
+        self.reader, self.writer = await asyncio.open_connection(
+            self.server_host, self.server_port)
+        await asyncio.gather(self.write_msg(), self.read_msg())
+
+    async def write_msg(self):
+        msg_to_write: str = ''
+        while msg_to_write != DISCONNECT:
+            message = await ainput('<Enter your message:> \n')  # Delete on prod
+            if message == DISCONNECT:
+                print('<Disconnecting...>')
+                self.disconnect = True
+                self.writer.close()
+                break
+            self.writer.write(message.encode())
+            await self.writer.drain()
+            print('<Message sent>')
+            await asyncio.sleep(0.1)  # Needed for task changing
+
+    async def read_msg(self):
+        msg_to_read: str = ''
+        while True:
+            if self.disconnect:
+                break
+            print('<Reading message...>')  # Delete on prod
+            msg_to_read = await self.reader.read(1024)
+            if msg_to_read:
+                print(f'<Received: {msg_to_read.decode()}>')
+        self.writer.close()
+
+
+if __name__ == '__main__':
+    client = Client()
+    asyncio.run(client.start_client())
--- server.py
+++ server.py
@@ -1,9 +1,64 @@
-# import asyncio
+import logging
+import asyncio
+from asyncio.streams import StreamReader, StreamWriter
+
+import app_logger
+from constants import HOST, PORT, PRIVATE
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 class Server:
-    def __init__(self, host="127.0.0.1", port=8000):
+    def __init__(self, host: str = HOST, port: int = PORT):
+        self.host: str = host
+        self.port: str = port
+        self.clients_dict: dict[str, tuple[StreamReader, StreamWriter]] = {}
+
+    async def connecting_clients(
+            self, reader: StreamReader, writer: StreamWriter) -> None:
+        address: tuple = writer.get_extra_info('peername')
+        client_host: str = address[0]
+        client_port: int = address[1]
+        client: str = client_host + str(client_port)
+        if client not in self.clients_dict.keys():
+            self.clients_dict[client] = (reader, writer)
+        logger.info('Client connected at %(host)s:%(port)s',
+                    {'host': client_host, 'port': client_port})
+
+        while True:
+            data: bytes = await reader.read(1024)
+            if not data:
+                break  # User disconnection
+            msg: str = data.decode()
+            if msg.startswith(PRIVATE):
+                self.private_msg(msg)
+            else:
+                await self.public_msg(msg)
+
+        logger.info('Client disconnected at %(host)s:%(port)s',
+                    {'host': client_host, 'port': client_port})
+        writer.close()
+
+    async def start(self) -> None:
+        srv = await asyncio.start_server(
+            self.connecting_clients, self.host, self.port)
+        logger.info('Server started at %(host)s:%(port)s',
+                    {'host': self.host, 'port': self.port})
+
+        async with srv:
+            await srv.serve_forever()
+
+    async def public_msg(self, msg_to_send: str) -> None:
+        for client in self.clients_dict.keys():
+            writer: StreamWriter = self.clients_dict[client][1]
+            writer.write(msg_to_send.encode())
+            await writer.drain()
+
+    def private_msg(self, msg_to_send: str) -> None:
         pass
 
-    def listen(self):
-        pass
+
+if __name__ == '__main__':
+    server = Server()
+    asyncio.run(server.start())
